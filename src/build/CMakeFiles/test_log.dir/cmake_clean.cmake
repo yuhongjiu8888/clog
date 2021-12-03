9f3fb5cddda76ab9082b4d766537db9485461bf7
yuwei_vendor <yuwei_vendor@senseauto.com>
2021-12-03 18:42:14 +0800

    first commit

---
+++ src/build/CMakeFiles/test_log.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_log.dir/log.c.o"
+  "CMakeFiles/test_log.dir/main.cpp.o"
+  "test_log.pdb"
+  "test_log"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test_log.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
